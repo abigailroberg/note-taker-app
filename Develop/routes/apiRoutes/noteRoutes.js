d6107b24c967dddf9ebff6ea8604f98b9ad0d4c5
Abby Roberg <abbyroberg@gmail.com>
2021-09-13 20:17:45 -0500

    added uniqid for new note posts

--- Develop/routes/apiRoutes/noteRoutes.js
+++ Develop/routes/apiRoutes/noteRoutes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const { notes } = require('../../db/db.json')
 const { filterByQuery, createNote } = require('../../lib/notes')
+const uniqid = require('uniqid')
 
 
 // get notes
@@ -15,7 +16,7 @@ router.get('/notes', (req, res) => {
 // post note
 router.post('/notes', (req, res) => {
     // set unique id
-    req.body.id = "id"
+    req.body.id = uniqid()
 
     const note = createNote(req.body, notes)
     res.json(note)
