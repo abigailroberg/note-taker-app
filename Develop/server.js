b3a06f942401ecd48b5bc2800db0983cd8e4633d
Abby Roberg <abbyroberg@gmail.com>
2021-09-13 19:45:22 -0500

    intial setup and basic get route

---
+++ Develop/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+const app = express()
+const PORT = process.env.PORT || 3001
+
+const apiRoutes = require('./routes/apiRoutes/noteRoutes')
+// const htmlRoutes = require('./routes/htmlRoutes/index')
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.static('public'))
+
+// use apiRoutes
+app.use('/api', apiRoutes)
+// app.use('/', htmlRoutes)
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`)
+})
